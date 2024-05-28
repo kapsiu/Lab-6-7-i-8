bbec545bc8334a6e17caecdd8d72a797e20a7be8
Bartosz Kurach <dsw51787@student.dsw.edu.pl>
2024-05-28 06:20:35 +0200

    Dodano wczytywanie oraz zapis do/z obiektu w formacie JSON

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
+import json
 import sys
+import yaml
+import xmltodict
+from xml.dom.minidom import parseString
+
+from dicttoxml import dicttoxml
 
 input_file = sys.argv[1]
 input_extension = sys.argv[1].split(".")[-1]
 target_file = sys.argv[2]
 target_extension = target_file.split(".")[-1]
+
+
+def convert_json(file, t_file, target_ext):
+    with open(file) as json_file:
+        data = json.load(json_file)
+
+    match target_ext:
+        case 'yaml' | "yml":
+            with open(t_file, "w") as outfile:
+                yaml.dump(data, outfile)
+        case "xml":
+            with open(t_file, "w") as outfile:
+                xml = dicttoxml(data)
+                dom = parseString(xml)
+                dump = dom.toprettyxml()
+                outfile.write(dump)
+        case _:
+            print("Conversion of the same type")
+            with open(t_file, "w") as outfile:
+                json.dump(data, outfile)
